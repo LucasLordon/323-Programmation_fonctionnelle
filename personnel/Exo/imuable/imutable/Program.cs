0595928ce86ab5c019c0e7e91e4416fc2d7b9600
LucasLordon <122774884+LucasLordon@users.noreply.github.com>
2024-10-02 12:05:01 +0200

    feat(silkroad exo)finish but not clean code

--- personnel/Exo/imuable/imutable/Program.cs
+++ personnel/Exo/imuable/imutable/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System.Linq;
-
+/*
 // 4 players
 List<Player> players = new List<Player>()
 {
@@ -9,7 +9,7 @@ List<Player> players = new List<Player>()
     new Player("Averell", 25)
 };
 
-/*// Initialize search
+// Initialize search
 Player elder = players.First();
 int biggestAge = elder.Age;
 
@@ -22,7 +22,7 @@ foreach (Player d in players)
         biggestAge = d.Age; // for future loops
     }
 }
-*/
+
 
 Player p = players.OrderBy(p => p.Age).Last();
 Console.WriteLine($"Le plus agé est {p.Name} qui a {p.Age} ans");
@@ -44,4 +44,73 @@ public class Player
     public string Name => _name;
 
     public int Age => _age;
+}*/
+
+
+using System;
+using System.Threading;
+
+class Program
+{
+    static int linesOfCode = 9210;
+    static Random random = new Random();
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"Opening shop with {linesOfCode} lines in our program");
+        Thread thread1 = new Thread(Bob);
+        Thread thread2 = new Thread(Alice);
+
+        // Start both threads
+        thread1.Start();
+        Thread.Sleep(300); // Alice starts her day a bit later
+        thread2.Start();
+
+        // Wait until both threads terminate
+        thread1.Join();
+        thread2.Join();
+
+        Console.WriteLine($"Closing shop with {linesOfCode} lines in our program");
+        Console.ReadLine();
+    }
+    public static int push(int lineOfCode,int editedLine)
+    {
+        lineOfCode += editedLine;
+        return lineOfCode;
+    }
+    static int Bob()
+    {
+        int myLineCounter = 0; // Bob starts working
+        int workingHours = 0;
+
+        while (workingHours < 9) // He has a 9-hours day ahead of him
+        {
+            Thread.Sleep(1000); // Bob works for "1 hour"
+            workingHours++;
+            int BobProduction = random.Next(10, 50);
+            Console.WriteLine($"Bob commits {BobProduction} lines of code.");
+            myLineCounter += BobProduction;
+        }
+        //linesOfCode = push(linesOfCode, myLineCounter);
+        Console.WriteLine($"Bob checks out, he claims the program has now {linesOfCode + myLineCounter} lines");
+        return myLineCounter;
+    }
+
+    // Method to be executed by thread2 - increments by 2 every 3 seconds
+    static int Alice()
+    {
+        int myLineCounter = 0; // Alice starts working
+        int workingHours = 0;
+        while (workingHours < 7) // She has a 7-hours day ahead of her
+        {
+            Thread.Sleep(1000); // Alice works for "1 hour"
+            workingHours++;
+            int AliceProduction = random.Next(20, 80);
+            Console.WriteLine($"Alice commits {AliceProduction} lines of code.");
+            myLineCounter += AliceProduction;
+        }
+        //linesOfCode = push(linesOfCode, myLineCounter);
+        Console.WriteLine($"Bob checks out, he claims the program has now {linesOfCode + myLineCounter} lines");
+        return myLineCounter;
+    }
 }
---
+++ personnel/Exo/silkroad/ConsoleApp1/Program.cs
@@ -0,0 +1,156 @@
+﻿const int BoardSize = 9;
+const int SilkyNumber = 20;
+Random rnd = new Random();
+bool[,] silkyWay = new bool[BoardSize, BoardSize];
+int[,] testedWay = new int[BoardSize, BoardSize];
+
+// testedWay :
+// 0 = not tested
+// 1 = tested
+// 2 = good way
+// 3 = detected silk (obstacle)
+
+for (int i = 0; i < SilkyNumber; i++)
+{
+    int y = rnd.Next(BoardSize);
+    int x = rnd.Next(BoardSize);
+    if ((x != BoardSize - 1) || (y != 0))
+    {
+        if (!silkyWay[x, y])
+        {
+            silkyWay[x, y] = true;
+        }
+        else
+        {
+            i--;
+        }
+    }
+    else
+    {
+        i--;
+    }
+}
+
+for (int i = 0; i < BoardSize; i++)
+{
+    for (int j = 0; j < BoardSize; j++)
+    {
+        testedWay[i, j] = silkyWay[i, j] ? 3 : 0;
+    }
+}
+
+void DrawBoard(bool[,] board)
+{
+    Console.Write("  ");
+    for (int i = 0; i < BoardSize; i++)
+    {
+        Console.Write(i + 1);
+    }
+    Console.Write("\n ┌");
+    for (int i = 0; i < BoardSize; i++)
+    {
+        Console.Write("─");
+    }
+    Console.WriteLine("┐");
+
+    for (int i = 0; i < BoardSize; i++)
+    {
+        Console.Write($"{(char)(i + 65)}│");
+        for (int j = 0; j < BoardSize; j++)
+        {
+            if (board[i, j])
+            {
+                Console.Write("█"); 
+            }
+            else
+            {
+                Console.Write(" "); 
+            }
+        }
+        Console.WriteLine("│");
+    }
+    Console.Write(" └");
+    for (int i = 0; i < BoardSize; i++)
+    {
+        Console.Write("─");
+    }
+    Console.WriteLine("┘");
+}
+
+bool FindPath(int[,] testedBoard, int x, int y)
+{
+    if (x == 0 && y == BoardSize-1)
+    {
+        testedBoard[x, y] = 2;
+        Console.WriteLine("Chemin trouvé !");
+        return true;
+    }
+
+    if (x < 0 || y < 0 || x >= BoardSize || y >= BoardSize)
+    {
+        return false;
+    }
+
+    if (testedBoard[x, y] != 0)
+    {
+        return false;
+    }
+
+    testedBoard[x, y] = 1;
+
+    if (FindPath(testedBoard, x - 1, y))
+    {
+        testedBoard[x, y] = 2; 
+        return true;
+    }
+
+    if (FindPath(testedBoard, x, y - 1))
+    {
+        testedBoard[x, y] = 2; 
+        return true;
+    }
+
+    if (FindPath(testedBoard, x + 1, y))
+    {
+        testedBoard[x, y] = 2;
+        return true;
+    }
+
+    if (FindPath(testedBoard, x, y + 1))
+    {
+        testedBoard[x, y] = 2;
+        return true;
+    }
+
+    return false;
+}
+
+DrawBoard(silkyWay);
+
+if (FindPath(testedWay, BoardSize - 1, 0))
+{
+    Console.WriteLine("Voici le chemin trouvé :");
+}
+else
+{
+    Console.WriteLine("Aucun chemin trouvé.");
+}
+
+void DrawPath(int[,] board)
+{
+    Console.WriteLine("\nChemin :");
+    for (int i = 0; i < BoardSize; i++)
+    {
+        for (int j = 0; j < BoardSize; j++)
+        {
+            if (board[i, j] == 2) Console.Write("O"); 
+            else if (board[i, j] == 3) Console.Write("█"); 
+            else Console.Write("."); 
+        }
+        Console.WriteLine();
+    }
+}
+
+DrawPath(testedWay);
+
+Console.ReadLine();
